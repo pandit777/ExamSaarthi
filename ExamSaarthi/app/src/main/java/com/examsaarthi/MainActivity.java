69bcc82505c63e0ba007d437dbee281f34c3fdc0
pandit777 <gs8901346287@gmail.com>
2025-12-07 11:09:37 +0530

    Add files via upload

---
+++ ExamSaarthi/app/src/main/java/com/examsaarthi/MainActivity.java
@@ -0,0 +1,36 @@
+package com.examsaarthi;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        web = new WebView(this);
+        setContentView(web);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("https://pandit777.github.io/pyq/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
